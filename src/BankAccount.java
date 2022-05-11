257a73ba13973cefe978cf15bf869f379144c187
Drew <drewbdoo@gmail.com>
2022-05-11 16:54:53 -0400

    this assignment sucks balls

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,20 +1,42 @@
-public class BankAccount {
-    private int balance;
-    private String name;
+import java.net.SecureCacheResponse;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-    public BankAccount(int balance, String customerName) {
+public class BankAccount {
+    private double balance;
+    private String name;
+    private double accountNumber;
+
+    public BankAccount(double balance, String customerName, double accountNumber) {
         this.balance = balance;
         this.name = customerName;
+        this.accountNumber = accountNumber;
     }
-    public int deposit(int x){
+
+    public double deposit(double x){
         return(balance+=x);
     };
-    public int withdrawal(int x){
+    public double withdrawal(double x){
         return (balance-=x);
     };
     public String getBalance(){
        return (name +"'s account balance: " + balance);
     };
+
+    public BankAccount(){
+        this.balance = 0;
+        this.name="";
+
+    }
+    public void setName(String x){
+        name = x;
+    }
+    public void setBalance(double x){
+        balance = x;
+    }
+public void setAccountNumber(int x){
+        accountNumber=x;
+}
 
 };
 
--- src/BankAccountMain.java
+++ src/BankAccountMain.java
@@ -1,15 +1,53 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class BankAccountMain {
+    static ArrayList<BankAccount> bankAccounts = new ArrayList<>();
     public static void main(String[] args) {
-//Step one, bank account creation
-        BankAccount bankAccount = new BankAccount(500, "Drew");
-        bankAccount.deposit(100);
-        System.out.println(bankAccount.getBalance());
-//Bank transfer
-        BankAccount bankAccount1 = new BankAccount(5000, "Sami");
-        BankAccount bankAccount2 = new BankAccount(300, "Dave");
-        bankAccount1.withdrawal(100);
-        bankAccount2.deposit(100);
-        System.out.println(bankAccount1.getBalance());
-        System.out.println(bankAccount2.getBalance());
+////Step one, bank account creation
+//        BankAccount bankAccount = new BankAccount(500, "Drew");
+//        bankAccount.deposit(100);
+//        System.out.println(bankAccount.getBalance());
+////Bank transfer
+//        BankAccount bankAccount1 = new BankAccount(5000, "Sami");
+//        BankAccount bankAccount2 = new BankAccount(300, "Dave");
+//        bankAccount1.withdrawal(100);
+//        bankAccount2.deposit(100);
+//        System.out.println(bankAccount1.getBalance());
+//        System.out.println(bankAccount2.getBalance());
+        Scanner scanner = new Scanner(System.in);
+        String reply;
+        do {
+            System.out.println("Hello world! Welcome to the Bank of Matt!");
+            System.out.println("Are you an existing customer? (-1 to exit)");
+            System.out.println("1. Yes");
+            System.out.println("2. No");
+            reply = scanner.nextLine();
+            if (reply.equalsIgnoreCase("1")) {
+                System.out.println("Good for you then");
+            } else if (reply.equalsIgnoreCase("2")) {
+                System.out.println("Let's make a new account!");
+                System.out.println("What is the name for the account");
+                String name = scanner.nextLine();
+                System.out.println("What is the beginning balance for the account");
+                String accountbal = scanner.nextLine();
+                double balance = Double.parseDouble(accountbal);
+                bankAccounts.add(new BankAccount());
+                (bankAccounts.get(bankAccounts.size()-1)).setAccountNumber(bankAccounts.size()-1);
+                (bankAccounts.get(bankAccounts.size()-1)).setName(name);
+                (bankAccounts.get(bankAccounts.size()-1)).setBalance(balance);
+            }else {
+            System.out.println("Not a valid response");
+        }
+    }while(!reply.equals("-1"));
+
     }
+
+
+
 }
+
+
+
