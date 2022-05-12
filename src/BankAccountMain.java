3cf40e99bab20f504e5bc106ded1c18619b9c746
Drew <34400283+drewbdoo@users.noreply.github.com>
2022-05-11 23:16:53 -0400

    testing home ssh

--- src/BankAccountMain.java
+++ src/BankAccountMain.java
@@ -4,6 +4,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class BankAccountMain {
+    //testing git ssh
     static ArrayList<BankAccount> bankAccounts = new ArrayList<>();
     public static void main(String[] args) {
 ////Step one, bank account creation
